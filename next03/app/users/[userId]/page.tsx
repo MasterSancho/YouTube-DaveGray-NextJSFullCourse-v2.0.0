20c6b9f0d6b0541fca33e2670b00b17f757cda3d
maltsevalex <57605252+MasterSancho@users.noreply.github.com>
2023-08-30 09:30:32 +0300

    004-->Chapter 4: SSG, SSR, ISR

--- next03/app/users/[userId]/page.tsx
+++ next03/app/users/[userId]/page.tsx
@@ -1,8 +1,11 @@
 import getUser from '@/lib/getUser';
 import getUserPosts from '@/lib/getUserPosts';
+import getAllUsers from '@/lib/getAllUsers';
 import { Suspense } from 'react';
 import UserPosts from './components/UserPosts';
 import type { Metadata } from 'next';
+
+import { notFound } from 'next/navigation';
 
 type Params = {
  params: {
@@ -15,6 +18,12 @@ export async function generateMetadata({
 }: Params): Promise<Metadata> {
  const userData: Promise<User> = getUser(userId);
  const user: User = await userData;
+
+ if (!user.name) {
+  return {
+   title: 'User Not Found',
+  };
+ }
 
  return {
   title: user.name,
@@ -30,6 +39,8 @@ export default async function UserPage({ params: { userId } }: Params) {
 
  const user = await userData;
 
+ if (!user.name) notFound();
+
  return (
   <>
    <h2>{user.name}</h2>
@@ -39,4 +50,11 @@ export default async function UserPage({ params: { userId } }: Params) {
    </Suspense>
   </>
  );
+}
+
+export async function generateStaticParams() {
+ const usersData: Promise<User[]> = getAllUsers();
+ const users = await usersData;
+
+ return users.map((user) => ({ userId: user.id.toString() }));
 }
